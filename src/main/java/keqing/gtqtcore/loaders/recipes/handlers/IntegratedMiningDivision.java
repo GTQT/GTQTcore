4e700b1f8b207221a633d1c3feeadecd90dcbfba
Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
2023-09-18 20:12:00 +0900

    A

--- src/main/java/keqing/gtqtcore/loaders/recipes/handlers/IntegratedMiningDivision.java
+++ src/main/java/keqing/gtqtcore/loaders/recipes/handlers/IntegratedMiningDivision.java
@@ -1,9 +1,11 @@
 package keqing.gtqtcore.loaders.recipes.handlers;
 
+import gregtech.api.GregTechAPI;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 import gregtech.api.recipes.ingredients.GTRecipeOreInput;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.properties.PropertyKey;
+import gregtech.api.unification.material.registry.MaterialRegistry;
 import gregtech.api.unification.ore.OrePrefix;
 import keqing.gtqtcore.api.event.SearchMaterialsClassEvent;
 
@@ -29,6 +31,7 @@ import static keqing.gtqtcore.api.unification.GTQTMaterials.HighPressureSteam;
 
 public class IntegratedMiningDivision {
     public static void init() {
+        addStaticRecipes();
 
         GTQTcoreRecipeMaps.INTEGRATED_MINING_DIVISION.recipeBuilder()
                 .fluidInputs(Naphtha.getFluid(1))
@@ -36,67 +39,38 @@ public class IntegratedMiningDivision {
                 .duration(10)
                 .EUt((int) V[LV])
                 .buildAndRegister();
-
-        for(var field : allMaterials()){
-            if(Modifier.isStatic(field.getModifiers())){
-                try {
-                    var obj = field.get(null);
-                    if(obj instanceof Material material
-                    ){
-
-                        var ingot = GTRecipeOreInput.getOrCreate(OrePrefix.ingot,material).getInputStacks();
-                        var plate = GTRecipeOreInput.getOrCreate(OrePrefix.plate,material).getInputStacks();
-                        var foil = GTRecipeOreInput.getOrCreate(OrePrefix.foil,material).getInputStacks();
-
-                        if(ingot.length>0 && plate.length>0 && foil.length>0){
-                            GTQTcoreRecipeMaps.INTEGRATED_MINING_DIVISION.recipeBuilder()
-                                    .EUt(24)
-                                    .input(OrePrefix.ingot,material)
-                                    .output(OrePrefix.foil,material,4)
-                                    .duration(Math.max((int)(material.getMass()*2.1),1))
-                                    .buildAndRegister();
-                        }
-                    }
-                }   catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
     }
 
     private static void addStaticRecipes() {
-        for(var field : allMaterials()){
-            if(Modifier.isStatic(field.getModifiers())){
-                try {
-                    var obj = field.get(null);
-                    if(obj instanceof Material material
-                    ){
-                        if (material.hasProperty(PropertyKey.ORE)) {
-                            addIntegratedMiningRecipe(material, 64)
-                                    .buildAndRegister();
-                        }
-                    }
-                }   catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
+        for (MaterialRegistry materialRegistry : GregTechAPI.materialManager.getRegistries())
+        {
+            for (Material material : materialRegistry) {
+                var ingot = GTRecipeOreInput.getOrCreate(OrePrefix.ingot,material).getInputStacks();
+                var plate = GTRecipeOreInput.getOrCreate(OrePrefix.plate,material).getInputStacks();
+                var foil = GTRecipeOreInput.getOrCreate(OrePrefix.foil,material).getInputStacks();
+
+                if(ingot.length>0 && plate.length>0 && foil.length>0){
+                    GTQTcoreRecipeMaps.INTEGRATED_MINING_DIVISION.recipeBuilder()
+                            .EUt(24)
+                            .input(OrePrefix.ingot,material)
+                            .output(OrePrefix.foil,material,4)
+                            .duration(Math.max((int)(material.getMass()*2.1),1))
+                            .buildAndRegister();
+                }
+
+                if (material.hasProperty(PropertyKey.ORE)) {
+                    addIntegratedMiningRecipe(material, 64)
+                            .EUt(128).duration(128)
+                            .buildAndRegister();
                 }
             }
         }
-    }
-
-    public static Collection<Field> allMaterials(){
-        var classList = new ArrayList<Class<?>>();
-        var event = new SearchMaterialsClassEvent(classList);
-        if(!MinecraftForge.EVENT_BUS.post(event)){
-            return event.getClassList().stream().flatMap(
-                    (clazz) -> Arrays.stream(clazz.getFields())).collect(Collectors.toList());
-        }
-        return Collections.emptySet();
     }
 
     private static SimpleRecipeBuilder addIntegratedMiningRecipe(Material material, int output) {
         return GTQTcoreRecipeMaps.INTEGRATED_MINING_DIVISION.recipeBuilder()
                 .input(OrePrefix.ore, material)
-                .output(OrePrefix.ingot, material, output);
+                .output(OrePrefix.dust, material, output);
     }
 
 }
