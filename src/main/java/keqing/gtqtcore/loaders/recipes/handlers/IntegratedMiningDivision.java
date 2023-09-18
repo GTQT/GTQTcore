62e9df4d6b9f8f3dbc95739dbddf94ed5c868b7d
KeQing <1158677939@qq.com>
2023-09-18 12:17:09 +0800

    开摆

--- src/main/java/keqing/gtqtcore/loaders/recipes/handlers/IntegratedMiningDivision.java
+++ src/main/java/keqing/gtqtcore/loaders/recipes/handlers/IntegratedMiningDivision.java
@@ -1,9 +1,11 @@
 package keqing.gtqtcore.loaders.recipes.handlers;
 
+import gregtech.api.GregTechAPI;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 import gregtech.api.recipes.ingredients.GTRecipeOreInput;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.properties.PropertyKey;
+import gregtech.api.unification.material.registry.MaterialRegistry;
 import gregtech.api.unification.ore.OrePrefix;
 import keqing.gtqtcore.api.event.SearchMaterialsClassEvent;
 
@@ -30,30 +32,40 @@ import static keqing.gtqtcore.api.unification.GTQTMaterials.HighPressureSteam;
 public class IntegratedMiningDivision {
     public static void init() {
 
-        GTQTcoreRecipeMaps.INTEGRATED_MINING_DIVISION.recipeBuilder()
-                .fluidInputs(Naphtha.getFluid(1))
-                .fluidOutputs(HighPressureSteam.getFluid(20))
-                .duration(10)
-                .EUt((int) V[LV])
-                .buildAndRegister();
-
-
-    }
-
-    private static void addStaticRecipes() {
+        addStaticRecipes();
         for(var field : allMaterials()){
             if(Modifier.isStatic(field.getModifiers())){
                 try {
                     var obj = field.get(null);
                     if(obj instanceof Material material
                     ){
-                        if (material.hasProperty(PropertyKey.ORE)) {
-                            addIntegratedMiningRecipe(material, 64)
+
+                        var ingot = GTRecipeOreInput.getOrCreate(OrePrefix.ingot,material).getInputStacks();
+                        var plate = GTRecipeOreInput.getOrCreate(OrePrefix.plate,material).getInputStacks();
+                        var foil = GTRecipeOreInput.getOrCreate(OrePrefix.foil,material).getInputStacks();
+
+                        if(ingot.length>0 && plate.length>0 && foil.length>0){
+                            GTQTcoreRecipeMaps.INTEGRATED_MINING_DIVISION.recipeBuilder()
+                                    .EUt(24)
+                                    .input(OrePrefix.ingot,material)
+                                    .output(OrePrefix.foil,material,4)
+                                    .duration(Math.max((int)(material.getMass()*2.1),1))
                                     .buildAndRegister();
                         }
                     }
                 }   catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private static void addStaticRecipes() {
+        for (MaterialRegistry materialRegistry : GregTechAPI.materialManager.getRegistries()){
+            for (Material material : materialRegistry) {
+                if (material.hasProperty(PropertyKey.ORE)) {
+                    addIntegratedMiningRecipe(material, 64)
+                            .buildAndRegister();
                 }
             }
         }
