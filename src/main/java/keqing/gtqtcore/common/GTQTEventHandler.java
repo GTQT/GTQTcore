1b7eaa8ce3cb5057d56d419d6afd015a503332ec
KeQing <1158677939@qq.com>
2024-03-18 12:02:23 +0800

    杀杀杀
    删除无线

--- src/main/java/keqing/gtqtcore/common/GTQTEventHandler.java
+++ src/main/java/keqing/gtqtcore/common/GTQTEventHandler.java
@@ -3,13 +3,11 @@ package keqing.gtqtcore.common;
 import gregtech.api.items.metaitem.StandardMetaItem;
 import gregtech.api.items.metaitem.stats.IItemBehaviour;
 import gregtech.api.unification.material.event.MaterialEvent;
-import keqing.gtqtcore.api.items.IExtendedItemBehavior;
 import keqing.gtqtcore.api.unification.GCYSMaterials;
 import keqing.gtqtcore.api.unification.GTQTMaterials;
 import keqing.gtqtcore.api.unification.OrePrefixAdditions;
 import keqing.gtqtcore.api.unification.TJMaterials;
-import keqing.gtqtcore.network.GCNetworkManager;
-import keqing.gtqtcore.network.packets.MouseEventToSeverPacket;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -28,33 +26,6 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class GTQTEventHandler {
 
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    @SideOnly(Side.CLIENT)
-    public static void onWheelEvent(MouseEvent event){
-        if(event.getDwheel() == 0){
-            return;
-        }
-
-        Minecraft minecraft = Minecraft.getMinecraft();
-        EntityPlayer player = minecraft.player;
-        if(player.isSneaking()){
-            ItemStack stack = player.getHeldItem(EnumHand.MAIN_HAND);
-            Item item = stack.getItem();
-            if(item instanceof StandardMetaItem){
-                StandardMetaItem metaItem = (StandardMetaItem) item;
-                for(IItemBehaviour behaviour : metaItem.getBehaviours(stack)){
-                    if(behaviour instanceof IExtendedItemBehavior){
-                        IExtendedItemBehavior extendedItemBehavior = (IExtendedItemBehavior) behaviour;
-                        if(extendedItemBehavior.canHandleWheelChange()){
-                            GCNetworkManager.INSTANCE.sendPacketToServer(new MouseEventToSeverPacket(event.getDwheel()>0));
-                            event.setCanceled(true);
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-    }
     public GTQTEventHandler() {
     }
 
