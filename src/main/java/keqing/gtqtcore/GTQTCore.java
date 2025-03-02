bbb14a51de938cbd2b0846e14b1f11e04a67936e
KeQing <1158677939@qq.com>
2025-03-02 13:14:24 +0800

    1

--- src/main/java/keqing/gtqtcore/GTQTCore.java
+++ src/main/java/keqing/gtqtcore/GTQTCore.java
@@ -30,11 +30,11 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
                 "after:gregtechfoodoption@[1.11.0,);"
 )
 public class GTQTCore {
-    public static final String PACK = "1.7.2";
+    public static final String PACK = "1.7.3";
 
     public static final String MODID = "gtqtcore";
     public static final String NAME = "GTQT Core";
-    public static final String VERSION = "0.1(2025/3/1)";
+    public static final String VERSION = "0.1(2025/3/15)";
 
     @Mod.Instance(GTQTCore.MODID)
     public static GTQTCore instance;
